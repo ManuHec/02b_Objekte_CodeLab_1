d6655ebdcb87ca841b8217f67c6a474737696992
Manuela <ellaheckenberger@gmail.com>
2021-12-02 16:32:49 +0100

    fertig

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,10 +1,23 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
-    
+        Konto konto1 = new Konto(10000);
+
+        // konto1.getkontostand();
+        System.out.println(konto1.getkontostand());
+
+        Konto konto2 = new Konto(10000 * 2);
+        System.out.println(konto2.getkontostand());
+
+        Konto konto3 = new Konto(10000 * 3);
+        System.out.println(konto3.getkontostand());
+
+        Konto konto4 = new Konto(10000 * 10);
+        System.out.println(konto4.getkontostand());
+
     }
 
 }
---
+++ src/com/cc/java/Konto.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Konto {
+
+    private int kontostand;
+
+    public Konto(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    public int getkontostand() {
+        return kontostand;
+    }
+
+}
