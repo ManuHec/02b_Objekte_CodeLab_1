98fbbf005052f7d48c12d2b84a8bff27b328bee6
Manuela <ellaheckenberger@gmail.com>
2021-12-03 09:19:04 +0100

    überarbeitet

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -7,16 +7,16 @@ public class App {
         Konto konto1 = new Konto(10000);
 
         // konto1.getkontostand();
-        System.out.println(konto1.getkontostand());
+        System.out.println(" Der aktuelle Kontostand beträgt:" + " " + konto1.getkontostand());
 
         Konto konto2 = new Konto(10000 * 2);
-        System.out.println(konto2.getkontostand());
+        System.out.println(" Ihr neuer Kontostand beträgt:" + " " + konto2.getkontostand());
 
         Konto konto3 = new Konto(10000 * 3);
-        System.out.println(konto3.getkontostand());
+        System.out.println(" Ihr neuer Kontostand beträgt:" + " " + konto3.getkontostand());
 
         Konto konto4 = new Konto(10000 * 10);
-        System.out.println(konto4.getkontostand());
+        System.out.println(" Ihr neuer Kontostand beträgt:" + " " + konto4.getkontostand());
 
     }
 
--- src/com/cc/java/Konto.java
+++ src/com/cc/java/Konto.java
@@ -3,6 +3,9 @@ package com.cc.java;
 public class Konto {
 
     private int kontostand;
+    public int konto2;
+    public int konto3;
+    public int konto4;
 
     public Konto(int kontostand) {
         this.kontostand = kontostand;
